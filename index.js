b590db5b800f4626b81e26bbf368434bf5bce687
Armaghan Orakzai <armaghan.orakzai15@gmail.com>
2021-03-28 20:22:18 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+
+! Warning, react component not showing
+
+? Why is the spinner color yellow
+
+TODO Make a authentication system
+TODO Integrate the google auth API 
+TODO Integrate the facebook auth API 
+
+*/
